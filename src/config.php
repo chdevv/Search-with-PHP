c018f679c764c279c11cee9ee4c5d4de592d144c
Chdevv <diesknot@gmail.com>
2024-01-08 19:48:24 -0300

    HOST NAME sql corrigido

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-const DB_HOST = "mysql:host=127.0.0.1:3306";
+const DB_HOST = "mysql:host=127.0.0.1:3306;";
 const DB_NAME = "dbname=sistema_busca;charset=utf8";
 const DB_USER = "user";
 const DB_PASSWORD = "root";
